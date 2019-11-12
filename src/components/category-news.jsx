4f03d89bcac5ebc015be703b5a3da002f1a21325
rromikas <rromikas@gmail.com>
2019-11-12 20:40:52 +0200

    istryniansd

--- src/components/category-news.jsx
+++ src/components/category-news.jsx
@@ -33,7 +33,7 @@ class CategoryNews extends Component {
       console.log(date);
 
       fetch(
-        `/news/v2/everything?qInTitle="${q}"&from=${date}&sortBy=publishedAt&language=en&apiKey=cf5f4e280b214b5285aa823a72b896cc`,
+        `https://newsapi.org/v2/everything?qInTitle="${q}"&from=${date}&sortBy=publishedAt&language=en&apiKey=cf5f4e280b214b5285aa823a72b896cc`,
         {
           method: "GET",
           headers: {
